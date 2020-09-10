0791ef3ec73c222324b3316c8473d9522a43323f
63110 <63110for@list.ru>
2020-09-10 20:10:30 +0300

    upd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+  "env": {
+      "browser": true,
+      "es2021": true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
+  "extends": "eslint:recommended",
+  "parserOptions": {
+      "ecmaVersion": 12,
+      "sourceType": "module"
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+  "rules": {
+      'no-console': 0,
+      'no-alert': 0,
+      'no-restricted-globals': 0,
+      'no-plusplus': 0,
+      'no-unused-vars': 0,
+  }
 };
